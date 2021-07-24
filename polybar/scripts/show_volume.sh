5984402fea1ffe0c7f4fedfe4e757a4d519e81a8
Michał <43048524+michaldzi@users.noreply.github.com>
2021-07-24 18:36:20 +0200

    Update show_volume.sh

--- polybar/scripts/show_volume.sh
+++ polybar/scripts/show_volume.sh
@@ -7,6 +7,8 @@ elif [ "$volume" -gt "50" ]; then
 	echo "  $volume"
 elif [ "$volume" == "0" ]; then
 	echo "  Muted"
+elif [ "$volume" = "Connection error" ]; then
+	echo "  Muted"
 else
 	echo "  $volume"
 fi
