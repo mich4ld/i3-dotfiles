106b8c68d86e937473e0ab5f4a6eb25d0386da65
Michał <43048524+michaldzi@users.noreply.github.com>
2021-07-24 12:34:06 +0200

    Update and rename volume.sh to volume/volume.sh

--- volume/volume.sh
+++ volume/volume.sh
@@ -1,7 +1,7 @@
 msgID="2345"
 volume="$(pamixer --get-volume)"
 
-if [ "$volume" -gt "65" ]; then
+if [ "$volume" -gt "50" ]; then
 	dunstify  "  $volume" -t 800 -r $msgID
 elif [ "$volume" == "0" ]; then
 	dunstify  "  Muted " -t 800 -r $msgID
